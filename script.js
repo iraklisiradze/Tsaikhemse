eb64d8d662578f471ca40d4334fd8c8bc114f3ce
iraklisiradze <kolkhy@gmail.com>
2020-07-31 23:03:30 +0400

    responsive_test1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let buttons = document.getElementsByClassName ("add");
 console.log(buttons.length);
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click",function() {
-    //alert(i);
+          
 this.classList.toggle("added");
 if (this.classList.contains("added")) {
     this.textContent="დამატებულია";
