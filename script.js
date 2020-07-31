ea97282c6c85c41de9939f96cbd645a9885fcd88
iraklisiradze <kolkhy@gmail.com>
2020-07-31 21:14:38 +0400

    javascript - added class

---
+++ script.js
@@ -0,0 +1,16 @@
+let buttons = document.getElementsByClassName ("add");
+console.log(buttons.length);
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click",function() {
+    //alert(i);
+this.classList.toggle("added");
+if (this.classList.contains("added")) {
+    this.textContent="დამატებულია";
+}
+else {
+    this.textContent="დამატება";
+}
+
+    } );
+    }
+    
